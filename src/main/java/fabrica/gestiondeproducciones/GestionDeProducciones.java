8884e1d9327a7080dd71103d37911dab25457acf
Maicol Bargas <maicolbargas@gmail.com>
2024-12-21 13:00:39 -0300

    Iniciailzar proyecto

---
+++ src/main/java/fabrica/gestiondeproducciones/GestionDeProducciones.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fabrica.gestiondeproducciones;
+
+/**
+ *
+ * @author maico
+ */
+public class GestionDeProducciones {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
